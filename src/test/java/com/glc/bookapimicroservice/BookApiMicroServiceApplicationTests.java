e99a645138ba4d28853218c019cd309d7b258db7
MehrXloop <mehrunissa.pirwani@xloopdigital.com>
2023-05-11 18:59:53 +0500

    first commit

---
+++ src/test/java/com/glc/bookapimicroservice/BookApiMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glc.bookapimicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
